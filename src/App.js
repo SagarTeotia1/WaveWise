a7d5b2ed3f47e2bd478bbb714caad948f15fabd2
SagarTeotia1 <147220406+SagarTeotia1@users.noreply.github.com>
2024-09-15 14:43:14 +0530

    commit--v

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import './App.css';
-import bgVideo from './assets/background-video.mp4'; // Add your video here
+import React from "react";
+import "./App.css";
+import bgVideo from "./assets/background-video.mp4"; // Add your video here
 
 function App() {
   return (
     <div className="App">
       {/* Navigation Bar */}
       <nav className="navbar">
-        <div className="navbar-left">WishWave</div>
+        <div className="navbar-left">Wave Wise</div>
+        <div className="navbar-center">
+          <button className="nav-button">GameS</button>
+          <button className="nav-button">Webinar</button>
+          <button className="nav-button">Meetups</button>
+          <button className="nav-button">Connect</button>
+          <button className="nav-button">Profile</button>
+        </div>
         <div className="navbar-right">
           <button className="login-button">Login</button>
         </div>
@@ -22,7 +29,7 @@ function App() {
 
       {/* Get Started Button */}
       <div className="button-container">
-        <button className="start-button">Get Started</button>
+        <button className="start-button">Tutorial !</button>
       </div>
     </div>
   );
