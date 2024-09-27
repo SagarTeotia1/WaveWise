4b08f9cb7a67bd9363daf5896221e1532699a600
SagarTeotia1 <147220406+SagarTeotia1@users.noreply.github.com>
2024-09-27 20:09:47 +0530

    commit final

--- GAMES/webinar env2/Assets/Auditorium/Shaders/EnvironmentMegaShaderInput.hlsl
+++
@@ -1,7 +0,0 @@
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-
-CBUFFER_START(UnityPerMaterial)
-    half4 _Color;
-    float4 _MainTex_ST;
-    half4 _ShadowColor;
-CBUFFER_END
